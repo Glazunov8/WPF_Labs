f55a1f572210cd51bdebab8d789cd7d88f32b1f5
Glazunov Egor <glaz836egor@mail.ru>
2019-10-08 20:53:18 +0300

    Add 5th labs

---
+++ Laba_5/Laba_5B/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba_5B
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BackgroundWorker backgroundWorker;
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            backgroundWorker = (BackgroundWorker)this.FindResource("backgroundWoker");
+        }
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (i < 1000) num.Text = e.ProgressPercentage.ToString();
+            if (i >= 1000) num.Text = (1000 - (Convert.ToInt32(e.ProgressPercentage.ToString()) - 999)).ToString();
+            progressBar.Value +=1;
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            for (; i < 2000; i++)
+            {
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    System.Threading.Thread.Sleep(5);
+                    worker.ReportProgress(i);
+                }
+            }
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+            start.IsEnabled = false;
+            stop.IsEnabled = true;
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void stop_Click(object sender, RoutedEventArgs e)
+        {
+            if (backgroundWorker.WorkerSupportsCancellation == true)
+            {
+                start.IsEnabled = true;
+                backgroundWorker.CancelAsync();
+                stop.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ Laba_5/Laba_5C/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laba_5C
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Cut_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void Copy_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+        private void Paste_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
